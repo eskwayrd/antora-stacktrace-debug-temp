e3c84b1183a844b1c4d2817362cb02384a3621e8
Ewan Edwards <eskwayrd@gmail.com>
2023-03-10 10:36:15 -0800

    Simplify, as much as possible, the broken extension.

--- broken_extension.js
+++ broken_extension.js
@@ -2,25 +2,10 @@
 
 'use strict'
 
-const extensionName = 'broken_extension'
-
-function register ({
-  config: {
-    ...unknownOptions
-  }
-}) {
-  const logger = this.getLogger(extensionName)
-
-  if (Object.keys(unknownOptions).length) {
-    const keys = Object.keys(unknownOptions)
-    throw new Error(
-      `Unrecognized option${keys.length !== 1 ? 's' : ''}` +
-      ` specified for ${extensionName}: ${keys.join(', ')}`
-    )
-  }
-
-  // During contentClassified, check all of the pages one by one
+function register ({ config: {} }) {
   this.on('contentClassified', ({ playbook, contentCatalog }) => {
+    // Code is broken here.
+    // Antora should report the next line, but does not.
     console.log('I am broken...
   })
 }
